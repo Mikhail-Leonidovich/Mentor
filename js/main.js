c81939a82cfd37367ec8a74eaab95d2200e32a9a
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-04-29 16:41:09 +0300

    Alex

--- js/main.js
+++ js/main.js
@@ -1,28 +1,3 @@
-// let inputOne = document.querySelector(".inputOne");
-// let inputTwo = document.querySelector(".inputTwo");
-
-// inputOne.addEventListener("keyup", first);
-// inputTwo.addEventListener("keyup", second);
-
-// function first() {
-//   localStorage.setItem("first", inputOne.value);
-//   localStorage.getItem("first");
-//   a = localStorage.getItem("first");
-// }
-
-// function second() {
-//   localStorage.setItem("second", inputTwo.value);
-//   localStorage.getItem("second");
-//   b = localStorage.getItem("second");
-// }
-
-// window.onload = function ready() {
-//   inputOne.value = localStorage.getItem("first");
-//   inputTwo.value = localStorage.getItem("second");
-// };
-
-// ===================================================
-
 let inputIn = document.querySelector(".inputIn");
 let labelOut = document.querySelector(".labelOut");
 
