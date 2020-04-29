d9005f8c7eedc1c2d290e874d6b985686957e3bb
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-04-29 16:42:55 +0300

    Maks

--- js/main.js
+++ js/main.js
@@ -1,34 +1,22 @@
-// let inputOne = document.querySelector(".inputOne");
-// let inputTwo = document.querySelector(".inputTwo");
+let inputOne = document.querySelector(".inputOne");
+let inputTwo = document.querySelector(".inputTwo");
 
-// inputOne.addEventListener("keyup", first);
-// inputTwo.addEventListener("keyup", second);
+inputOne.addEventListener("keyup", first);
+inputTwo.addEventListener("keyup", second);
 
-// function first() {
-//   localStorage.setItem("first", inputOne.value);
-//   localStorage.getItem("first");
-//   a = localStorage.getItem("first");
-// }
-
-// function second() {
-//   localStorage.setItem("second", inputTwo.value);
-//   localStorage.getItem("second");
-//   b = localStorage.getItem("second");
-// }
-
-// window.onload = function ready() {
-//   inputOne.value = localStorage.getItem("first");
-//   inputTwo.value = localStorage.getItem("second");
-// };
-
-// ===================================================
-
-let inputIn = document.querySelector(".inputIn");
-let labelOut = document.querySelector(".labelOut");
-
-inputIn.addEventListener("keyup", One);
-
-function One() {
-  localStorage.setItem("first", inputIn.value);
-  labelOut.innerHTML = localStorage.getItem("first");
+function first() {
+  localStorage.setItem("first", inputOne.value);
+  localStorage.getItem("first");
+  a = localStorage.getItem("first");
 }
+
+function second() {
+  localStorage.setItem("second", inputTwo.value);
+  localStorage.getItem("second");
+  b = localStorage.getItem("second");
+}
+
+window.onload = function ready() {
+  inputOne.value = localStorage.getItem("first");
+  inputTwo.value = localStorage.getItem("second");
+};
