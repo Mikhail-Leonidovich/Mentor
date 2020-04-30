54e0859cd68c72a02a4b65d7c1ec13316865017c
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-04-30 18:11:41 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
 let inputOne = document.querySelector(".inputOne");
 let inputTwo = document.querySelector(".inputTwo");
-
-inputOne.addEventListener("keyup", listenerFunction);
-inputTwo.addEventListener("keyup", listenerFunction);
+let button = document.querySelector(".btn-1");
 
 let onlyForm = document.forms.onlyForm;
 let firstInput = onlyForm.elements.first;
 let secondInput = onlyForm.elements.second;
+let anotherForm = document.forms.anotherForm;
 
-function listenerFunction() {
+inputOne.addEventListener("keyup", () => {
   localStorage.setItem("a", firstInput.value);
+});
+inputTwo.addEventListener("keyup", () => {
   localStorage.setItem("b", secondInput.value);
-}
+});
 
-// =========================================
+onlyForm.onsubmit = (e) => {
+  e.preventDefault();
+  if (inputOne.value === "Михаил" && inputTwo.value === "Юркевич") {
+    anotherForm.style.display = "block";
+  } else {
+    anotherForm.style.display = "none";
+  }
+};
 
-window.onload = function () {
+window.addEventListener("onload", () => {
   inputOne.value = localStorage.getItem("a");
   inputTwo.value = localStorage.getItem("b");
-};
+});
