c86080ace9c521710d0f96877666494de487aa53
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-06-16 14:18:00 +0300

    Name

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
-let btnNameOne = document.querySelector(".buttonOne");
-let btnNameTwo = document.querySelector(".buttonTwo");
+// let btnNameOne = document.querySelector(".buttonOne");
+// let btnNameTwo = document.querySelector(".buttonTwo");
 
-function onOperationButtonClick(eventObject) {
-  let clickElement = eventObject.currentTarget;
-  let btnName = clickElement.name;
-  console.log(btnName);
-}
+// function onOperationButtonClick(eventObject) {
+//   let clickElement = eventObject.currentTarget;
+//   let btnName = clickElement.name;
+//   console.log(btnName);
+// }
 
-btnNameOne.addEventListener("click", onOperationButtonClick);
-btnNameTwo.addEventListener("click", onOperationButtonClick);
+// btnNameOne.addEventListener("click", onOperationButtonClick);
+// btnNameTwo.addEventListener("click", onOperationButtonClick);
+
+
+
+document.querySelector(".buttonOne").onclick = (event) => {
+  console.log(event.target.name);
+};
+
+// Это простой способ, а сверху немного усложненный, с выбором из двух кнопок.
