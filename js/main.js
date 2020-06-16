1d05762a5c6c8e18c7cafe1e2dea7fde58b0566c
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-06-16 13:52:08 +0300

    Name

--- js/main.js
+++ js/main.js
@@ -1,28 +1,11 @@
-let inputOne = document.querySelector(".inputOne");
-let inputTwo = document.querySelector(".inputTwo");
-let button = document.querySelector(".btn-1");
+let btnNameOne = document.querySelector(".buttonOne");
+let btnNameTwo = document.querySelector(".buttonTwo");
 
-let onlyForm = document.querySelector(".only-form");
-let anotherForm = document.querySelector(".second-form");
+function onOperationButtonClick(eventObject) {
+  let clickElement = eventObject.currentTarget;
+  let btnName = clickElement.name;
+  console.log(btnName);
+}
 
-inputOne.addEventListener("keyup", () => {
-  localStorage.setItem("a", inputOne.value);
-});
-inputTwo.addEventListener("keyup", () => {
-  localStorage.setItem("b", inputTwo.value);
-});
-
-onlyForm.addEventListener("onsubmit", (e) => {
-  e.preventDefault();
-  if (inputOne.value === "Михаил" && inputTwo.value === "Юркевич") {
-    anotherForm.style.display = "block";
-  } else {
-    anotherForm.style.display = "none";
-  }
-});
-
-// need to fix
-window.onload = () => {
-  inputOne.value = localStorage.getItem("a");
-  inputTwo.value = localStorage.getItem("b");
-};
+btnNameOne.addEventListener("click", onOperationButtonClick);
+btnNameTwo.addEventListener("click", onOperationButtonClick);
