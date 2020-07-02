28d2e9bc9a5f5138d88a209f09142dfd5ce679d0
Mikhail Yurkevich <mikhail.yurkevich.blr@gmail.com>
2020-07-02 13:19:06 +0300

    combo

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let inputInOne = document.querySelector(".inputInOne");
 let inputInTwo = document.querySelector(".inputInTwo");
 let inputInThree = document.querySelector(".inputInThree");
+let form = document.querySelector(".form");
 
 inputInOne.addEventListener("keyup", onOperationButtonClick);
 inputInTwo.addEventListener("keyup", onOperationButtonClick);
@@ -17,5 +18,24 @@ window.addEventListener("load", () => {
   for (let elem of elements) {
     let a = localStorage.getItem(elem.name);
     elem.value = a;
+  }
+});
+
+form.addEventListener("submit", (e) => {
+  let vision = document.querySelector(".content");
+  vision.style.visibility = "visible";
+  e.preventDefault();
+
+  let elementsInp = document.querySelectorAll("input");
+  let elementsLab = document.querySelectorAll("label");
+  let i = 0;
+
+  for (let elemInp of elementsInp) {
+    let a = localStorage.getItem(elemInp.name);
+    for (; i < elementsLab.length; ) {
+      elementsLab[i].innerHTML = a;
+      break;
+    }
+    i++;
   }
 });
